297e02b5bd10b535d5beccc93d1b94e2ed3918a0
Mopati Bogatsu <mopatib@gmail.com>
2019-05-18 02:07:42 -0400

    updated appropriate file upload

--- routes/config.js
+++
@@ -1,4 +0,0 @@
-var devDatabase = 'mongodb://localhost:27017/endevour';
-var productionDatabase = "mongodb://Sparksiano:keeperOfTrack07@ds135776.mlab.com:35776/endevour";
-
-exports.url = devDatabase;
